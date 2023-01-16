af9c72db7fc28871e6574c9c3fd0697b221b2afe
m5c <mailtomainak@gmail.com>
2023-01-16 22:44:48 +0100

    Initial checkin

---
+++ README.md
@@ -0,0 +1,5 @@
+# snippetbox
+
+
+<h3> Snippet Box</h3>
+<p> Following the book by Alex Edwards and will get a web app up and running.</p>
---
+++ cmd/web/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"os"
+)
+
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
+func main() {
+
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.LUTC)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.LUTC|log.Lshortfile)
+
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
+
+	infoLog.Printf("Starting server on %s", *addr)
+
+	server := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
+	err := server.ListenAndServe()
+	errorLog.Fatal(err)
+}
