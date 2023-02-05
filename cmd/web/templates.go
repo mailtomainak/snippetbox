c7d73a635728438e726eeea23ef5b289fbb740b7
m5c <mailtomainak@gmail.com>
2023-02-05 22:44:21 +0100

    add cache for templates

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,8 +1,37 @@
 package main
 
-import "github.com/mailtomainak/snippetbox/pkg/models"
+import (
+	"github.com/mailtomainak/snippetbox/pkg/models"
+	"html/template"
+	"path/filepath"
+)
 
 type templateData struct {
 	Snippet  *models.Snippet
 	Snippets []*models.Snippet
+}
+
+func newTemplateCache(dir string) (map[string]*template.Template, error) {
+	templateCache := make(map[string]*template.Template)
+	pages, err := filepath.Glob(filepath.Join(dir, "*.page.tmpl"))
+	if err != nil {
+		return nil, err
+	}
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.ParseFiles(page)
+		if err != nil {
+			return nil, err
+		}
+		ts, err = ts.ParseGlob(filepath.Join(dir, "*.layout.tmpl"))
+		if err != nil {
+			return nil, err
+		}
+		ts, err = ts.ParseGlob(filepath.Join(dir, "*.partial.tmpl"))
+		if err != nil {
+			return nil, err
+		}
+		templateCache[name] = ts
+	}
+	return templateCache, nil
 }
